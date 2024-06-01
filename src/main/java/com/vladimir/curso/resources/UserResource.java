7b882312763c10197f5e4418fa75991d517cee35
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-06-01 20:48:14 -0300

    User entity and resource

---
+++ src/main/java/com/vladimir/curso/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.vladimir.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vladimir.curso.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User user = new User(1, "vladimir", "teste@gmail.com", "519933", "12344");
+        return ResponseEntity.ok().body(user);
+    }
+    
+}
