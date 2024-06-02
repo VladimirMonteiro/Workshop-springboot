91391b1285ce1b4870bdf18fc240a7a26d0812fa
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-06-01 21:40:13 -0300

    h2 database, test profile, jpa

--- src/main/java/com/vladimir/curso/entities/User.java
+++ src/main/java/com/vladimir/curso/entities/User.java
@@ -2,8 +2,20 @@ package com.vladimir.curso.entities;
 
 import java.io.Serializable;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "db_user")
 public class User implements Serializable{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
