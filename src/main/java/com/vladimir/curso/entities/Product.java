4eb904cbb438eb8e041d7eb564a014e6ea22f4dc
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-06-05 18:55:50 -0300

    product order item associatios

--- src/main/java/com/vladimir/curso/entities/Product.java
+++ src/main/java/com/vladimir/curso/entities/Product.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,33 +13,32 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
     private Double price;
     private String imgUrl;
 
-
     @ManyToMany
-    @JoinTable(name = "tb_product_category", 
-    joinColumns = @JoinColumn(name = "product_id"),
-    inverseJoinColumns =@JoinColumn(name = "category_id"))
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
-    public Product(){
+    @OneToMany(mappedBy = "id.product")
+  
+    private Set<OrderItem> items = new HashSet<>();
+
+    public Product() {
 
     }
-
-    
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -47,10 +46,8 @@ public class Product implements Serializable {
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
-      
+
     }
-
-
 
     public Long getId() {
         return id;
@@ -96,7 +93,14 @@ public class Product implements Serializable {
         return categories;
     }
 
-
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x: items){
+            set.add(x.getOrder());
+        }
+        return set;
+    }
 
     @Override
     public int hashCode() {
@@ -105,8 +109,6 @@ public class Product implements Serializable {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
-
-
 
     @Override
     public boolean equals(Object obj) {
@@ -124,5 +126,5 @@ public class Product implements Serializable {
             return false;
         return true;
     }
-    
+
 }
